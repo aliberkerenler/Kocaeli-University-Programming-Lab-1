a1544e878cf0f287be909acccb569aceabbf0538
Berke <aliberkerenler@gmail.com>
2024-10-19 17:35:59 +0300

    Update saldiri_gucu_hesapla.c

--- Codes/saldiri_gucu_hesapla.c
+++ Codes/saldiri_gucu_hesapla.c
@@ -1,12 +1,14 @@
-void saldiri_gucu_hesapla(Human_Unit *HU, Ork_Unit *OU,  int *toplam_insan_saldiri, int *toplam_ork_saldiri) {
-    
-    *toplam_insan_saldiri = (HU->piyadeler.saldiri * HU->piyadeler.sayi) +
-                            (HU->okcular.saldiri * HU->okcular.sayi) +     
-                            (HU->suvariler.saldiri * HU->suvariler.sayi) +
-                            (HU->kusatma_makineleri.saldiri * HU->kusatma_makineleri.sayi); 
+void saldiri_gucu_hesapla(Human_Unit *HU, Ork_Unit *OU , int adim , double *toplam_insan_saldiri , double *toplam_ork_saldiri)
+{
+    *toplam_insan_saldiri = kritik_vurus_hesapla(&HU->piyadeler , adim) + 
+                            kritik_vurus_hesapla(&HU->okcular , adim) + 
+                            kritik_vurus_hesapla(&HU->suvariler , adim) + 
+                            kritik_vurus_hesapla(&HU->kusatma_makineleri , adim) ;
 
-    *toplam_ork_saldiri = (OU->ork_dovusculeri.saldiri * OU->ork_dovusculeri.sayi) + 
-                          (OU->mizrakcilar.saldiri * OU->mizrakcilar.sayi) +     
-                          (OU->varg_binicileri.saldiri * OU->varg_binicileri.sayi) + 
-                          (OU->troller.saldiri * OU->troller.sayi);           
+    
+    *toplam_ork_saldiri =   kritik_vurus_hesapla(&OU->ork_dovusculeri , adim) +
+                            kritik_vurus_hesapla(&OU->mizrakcilar , adim) +
+                            kritik_vurus_hesapla(&OU->varg_binicileri , adim) +
+                            kritik_vurus_hesapla(&OU->troller , adim);
+   
 }
