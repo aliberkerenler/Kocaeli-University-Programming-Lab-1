9051503ce5f501435729642db75716349f7c47d2
Berke <aliberkerenler@gmail.com>
2024-10-22 01:51:13 +0300

    Update kritik_vurus_hesapla.c

--- Codes/kritik_vurus_hesapla.c
+++ Codes/kritik_vurus_hesapla.c
@@ -1,8 +1,13 @@
 int kritik_vurus_hesapla(Unit *unit, int adim)
 {
     // Birimin kritik vuruş zamanını hesapla
+    if(unit->kritik_sans == 0)
+    {
+        return unit->saldiri * unit->sayi;
+    }
+
     if (adim % (100 / unit->kritik_sans) == 0)
-    { 
+    {
         return unit->saldiri * unit->sayi * 1.5; // Kritik vuruş varsa hasarı artır
     }
 
