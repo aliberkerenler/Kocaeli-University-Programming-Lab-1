8030f26d8dd0438dd3d5c7315ddd9f93acb929f9
Berke <aliberkerenler@gmail.com>
2024-11-13 23:26:08 +0300

    Delete Codes directory

--- Codes/insan_net_hasar_hesaplama.c
+++
@@ -1,7 +0,0 @@
-int insan_net_hasar_hesaplama(Human_Unit *HU, int *toplam_insan_saldiri, int *toplam_insan_savunma, int *toplam_ork_savunma)
-{
-    int insan_net_hasar = *toplam_insan_saldiri * (1 - (*toplam_ork_savunma / *toplam_insan_saldiri));
-    if (insan_net_hasar < 0)
-        insan_net_hasar = 0;
-    return (insan_net_hasar);
-}
--- Codes/ork_net_hasar_hesaplama.c
+++
@@ -1,7 +0,0 @@
-int ork_net_hasar_hesaplama(Ork_Unit *OU, int *toplam_ork_saldiri, int *toplam_ork_savunma, int *toplam_insan_savunma)
-{
-    int ork_net_hasar = *toplam_ork_saldiri * (1 - (*toplam_insan_savunma / *toplam_ork_saldiri));
-    if (ork_net_hasar < 0)
-        ork_net_hasar = 0;
-    return (ork_net_hasar);
-}
